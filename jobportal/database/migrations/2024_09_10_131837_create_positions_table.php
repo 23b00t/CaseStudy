a0246f2b632b60890745dfc889494291bcdad528
Daniel Kipp <daniel.kipp@gmail.com>
2024-09-10 15:27:33 +0200

    Migrationen erfolgreich durchgeführt

--- jobportal/database/migrations/2024_09_10_131837_create_positions_table.php
+++ jobportal/database/migrations/2024_09_10_131837_create_positions_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
+            $table->string('title', length: 255);
+            $table->text('description');
+            $table->string('location', length: 256);
+            $table->integer('salary');
+            // create foreign key constraineds by naming convention
+            // Delete a job if the company was deleted
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            // Set category to null if it was deleted
+            $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
--- jobportal/database/migrations/2024_09_10_131849_create_companies_table.php
+++ jobportal/database/migrations/2024_09_10_131849_create_companies_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name', length: 255);
+            $table->text('description');
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
