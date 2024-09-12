d6bc07f4c3a59542386b175ea5abc8a766a03045
Daniel Kipp <daniel.kipp@gmail.com>
2024-09-12 14:47:37 +0200

    starter kit installiert

--- jobportal/routes/web.php
+++ jobportal/routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('positions', App\Http\Controllers\PositionController::class);
-Route::resource('companys', App\Http\Controllers\CompanyController::class);
-Route::resource('categorys', App\Http\Controllers\CategoryController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
