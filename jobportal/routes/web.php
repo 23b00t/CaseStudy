a9853becf91130ff795d1d303b6f5e770356ccce
Daniel Kipp <daniel.kipp@gmail.com>
2024-09-12 10:24:50 +0200

    Routes für Positions, Companys und Categorys hinzugefügt

--- jobportal/routes/web.php
+++ jobportal/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResources([
+    'positions' => PositionController::class,
+    'companys' => CompanyController::class,
+    'categorys' => CategoryController::class,
+]);
