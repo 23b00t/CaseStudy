9c29de9ce7f19ae252c1777a2e995395712da889
Daniel Kipp <daniel.kipp@gmail.com>
2024-09-12 12:39:09 +0200

    app.blade.php als template erzeugt, inkl. bootstrap navbar; positions/index angepasst

--- jobportal/resources/views/positions/index.blade.php
+++ jobportal/resources/views/positions/index.blade.php
@@ -1,28 +1,36 @@
-<table class="table table-striped table-bordered">
-    <thead>
-        <tr>
-            <td>Titel</td>
-            <td>Ort</td>
-        </tr>
-    </thead>
-    <tbody>
-    @foreach($allPositions as $key => $value)
-        <tr>
-            <td>{{ $value->title }}</td>
-            <td>{{ $value->location }}</td>
+@extends('layouts.app')
 
-            <td>
-                <form action="{{ url('positions/' . $value->id) }}" method="POST" class="pull-right">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-warning">Jobangebot löschen</button>
-                </form>
+@section('title', 'Positionen')
 
-                <a class="btn btn-small btn-success" href="{{ url('positions/' . $value->id) }}">Angebot anzeigen</a>
+@section('content')
+    <h1>Liste der Positionen</h1>
+    <table class="table table-striped table-bordered">
+        <thead>
+            <tr>
+                <td>Titel</td>
+                <td>Ort</td>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($allPositions as $key => $value)
+            <tr>
+                <td>{{ $value->title }}</td>
+                <td>{{ $value->location }}</td>
 
-                <a class="btn btn-small btn-info" href="{{ url('positions/' . $value->id . '/edit') }}">Angebot bearbeiten</a>
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+                <td>
+                    <form action="{{ url('positions/' . $value->id) }}" method="POST" class="pull-right">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-warning">Jobangebot löschen</button>
+                    </form>
+
+                    <a class="btn btn-small btn-success" href="{{ url('positions/' . $value->id) }}">Angebot anzeigen</a>
+
+                    <a class="btn btn-small btn-info" href="{{ url('positions/' . $value->id . '/edit') }}">Angebot bearbeiten</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endsection
+
