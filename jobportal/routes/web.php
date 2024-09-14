3764a8d4a73f15a50bf0d7e4fb6deaa3b785d99c
Daniel Kipp <daniel.kipp@gmail.com>
2024-09-14 14:57:30 +0200

    Authentifizierungsmiddleware auf alle Routen angewandt; sitemap aktualisiert

--- jobportal/routes/web.php
+++ jobportal/routes/web.php
@@ -11,15 +11,18 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Use of authentification middleware
 Route::middleware('auth')->group(function () {
+    // Auto-generated Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // CRUD routes
+    Route::resource('positions', App\Http\Controllers\PositionController::class);
+    Route::resource('companys', App\Http\Controllers\CompanyController::class);
+    Route::resource('categorys', App\Http\Controllers\CategoryController::class);
 });
 
-// CRUD routes
-Route::resource('positions', App\Http\Controllers\PositionController::class);
-Route::resource('companys', App\Http\Controllers\CompanyController::class);
-Route::resource('categorys', App\Http\Controllers\CategoryController::class);
 
 require __DIR__.'/auth.php';
